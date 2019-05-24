ea000767f6da256af3da431470f5c8634b369e1b
Andrea Rochira <roc16012@byui.edu>
2019-05-24 15:17:32 -0700

    Topic #7 - adjustments

--- CIT261/js/topic7.js
+++ CIT261/js/topic7.js
@@ -1,4 +1,24 @@
 
+// target a single element, <h3> tag
+var heading = document.getElementById('header3');
+
+// use an event listener to trigger the change in style
+heading.addEventListener('mouseover',function(){
+
+    // set the style properties to apply
+    var look = "text-align: center; font-size: 2em; color: #d2691e;";
+
+    // apply CSS rule
+    heading.style.cssText = look;
+});
+
+heading.addEventListener('mouseout',function(){
+
+    // remove any CSS style
+    heading.style.cssText = 'none';
+});
+
+
 // target the class of elements (label tags)
 var labels = document.querySelectorAll('.lab');
 
@@ -40,16 +60,17 @@ function unStyleForm(){
         labels[i].style ='none';
     }
 
-    // remove any style from fields
+    // remove any style from fields, a different way to achieve the goal
     for(var i = 0; i < fields.length; i++){
         fields[i].removeAttribute("style");
     }
 
 }
 
-
+// target the class of elements (<li> tags)
 var lis = document.querySelectorAll('.bullet');
 
+// apply stile to all the <li> tags
 function styleList() {
 
     // set the style of the class of HTML elements in a CSS fashion
@@ -69,15 +90,28 @@ function styleList() {
     for(var i = 0; i < lis.length; i++){
         lis[i].style.cssText = lisCSS;
     }
-
-
 }
 
+// remove the stile from all the <li> tags
 function unStyleList() {
 
     // remove any style from bullet list
     for(var i = 0; i < lis.length; i++){
         lis[i].style ='none';
     }
+}
 
+// select HTML element to use in the following functions
+var frame = document.getElementById('zoom');
+
+// on mouse hover zoom the thumbnail in the bigger container
+function zoomIn(element){
+
+    // place image as background of the <section> tag
+    frame.style.backgroundImage = "url("+element.src+")";
+}
+
+// on mouse leave, remove the image
+function zoomOut(element){
+    frame.style.backgroundImage = "";
 }
