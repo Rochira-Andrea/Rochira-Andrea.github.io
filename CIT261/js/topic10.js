b5c73eb54d8fea0fb6dd2c4124331bfc5fabfcb2
Andrea Rochira <roc16012@byui.edu>
2019-05-24 20:02:34 -0700

    Topic #10 - draft

---
+++ CIT261/js/topic10.js
@@ -0,0 +1,11 @@
+
+// target the canvas tag
+var canvas = document.getElementById('graphic');
+
+// set the context
+var c = canvas.getContext('2d');
+
+// draw some squares
+c.fillRect(50, 50, 50, 50);
+
+
