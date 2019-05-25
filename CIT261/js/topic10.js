9d911a098f69d589405b142bb20b9637a9246de8
Andrea Rochira <roc16012@byui.edu>
2019-05-25 11:49:23 -0700

    Topic 7 & 10 - adjustments

--- CIT261/js/topic10.js
+++ CIT261/js/topic10.js
@@ -1,11 +1,45 @@
 
-// target the canvas tag
-var canvas = document.getElementById('graphic');
+// target the first canvas tag
+var canvasOne = document.getElementById('graphic1');
 
-// set the context
-var c = canvas.getContext('2d');
+// set the context fro canvas #1
+var cst = canvasOne.getContext('2d');
 
-// draw some squares
-c.fillRect(50, 50, 50, 50);
+// draw a square
+cst.fillStyle = "#edeee9";
+cst.fillRect(137.5, 62.5, 25, 25);
+
+// add a border to the square
+cst.beginPath();
+cst.moveTo(175,50);
+cst.lineTo(125,50);
+cst.lineTo(125,100);
+cst.lineTo(175,100);
+cst.lineTo(175,50);
+cst.strokeStyle = '#d2691e';
+cst.stroke();
+
+// draw some circles
+cst.beginPath();
+cst.arc(95,75,40,0, Math.PI * 2,false);
+cst.strokeStyle = "#628395";
+cst.stroke();
+
+cst.beginPath();
+cst.arc(205,75,40,0, Math.PI * 2,false);
+cst.strokeStyle = "#628395";
+cst.stroke();
+
+cst.beginPath();
+cst.arc(150,75,40,0, Math.PI * 2,false);
+cst.strokeStyle = "#628395";
+cst.stroke();
 
 
+// target the second canvas
+var canvasTwo = document.getElementById('graphic2');
+
+// set the context for canvas #2
+var cnd = canvasTwo.getContext('2d');
+cnd.font = "45px Fredoka One";
+cnd.strokeText("Andrea",85,80);
