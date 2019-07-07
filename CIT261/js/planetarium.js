2dccabe8d58be446ea7974d15596efa0b9b6de59
Andrea Rochira <roc16012@byui.edu>
2019-07-06 21:47:40 -0700

    I miss just an icon and it's over!

--- CIT261/js/planetarium.js
+++ CIT261/js/planetarium.js
@@ -54,6 +54,9 @@ submit.addEventListener('click',function(){
 
     if(input === ""){
         document.getElementById('hint').innerText = "Please enter or select a name";
+
+    // you may want to add another condition here to handle mistyped names
+
     }else {
 
         // create an instance of XMLHttpRequest
@@ -66,6 +69,7 @@ submit.addEventListener('click',function(){
         } else if (input === "Ultima Thule") {
             input = "ultima-thule";
         }
+
         // establish the connection
         request.open('GET', 'https://api.le-systeme-solaire.net/rest.php/bodies/' + input);
 
@@ -77,7 +81,7 @@ submit.addEventListener('click',function(){
             var data = JSON.parse(request.responseText);
 
             // print the parsed data into the console (for debugging purpose)
-            console.log(data);
+            //console.log(data);
 
             // call for the function responsible to display the data in the html page
             fillHTMLelement(data);
@@ -89,20 +93,125 @@ submit.addEventListener('click',function(){
 
 
 function fillHTMLelement(jstring){
-    document.getElementById('bottomBtn').style.cssText = "display: flex; flex-flow: row nowrap; justify-content: center; position:absolute; bottom:1em; left:47.5%; animation: upDownBtn 3s linear infinite;";
+    document.getElementById('bottomBtn').style.cssText = "display: flex; flex-flow: row nowrap; justify-content: center; position:absolute; bottom:4em; left:47.5%; animation: upDownBtn 2s linear infinite;";
     document.getElementById('display-response').style.cssText = "display: flex; flex-flow: row wrap; align-items: center; justify-content: space-evenly; width: 100vw; height: 100vh;";
-    document.getElementById('body-name').innerText = jstring.englishName;
+    document.getElementById('hint').innerText = "Great! Now scroll down or click on the arrow";
+    document.getElementById('hint').style.visibility = "visible";
+    let tiles = document.querySelectorAll('.tile');
 
+    tiles.forEach(flipBack);
+    function flipBack(el){
+        el.style.cssText = "transform: none; transition: transform .3s linear";
+    }
 
+    // make sure to empty any child node within the information tiles
+    let backs = document.querySelectorAll('.back');
+
+    backs.forEach(clean);
+
+    function clean(back){
+        while (back.firstChild) {
+            back.removeChild(back.firstChild);
+        }
+    }
+
+    /* handle heavenly body name in the Name tile */
+    if(jstring.englishName !== "") {
+        document.getElementById('body-name').innerText = jstring.englishName;
+    } else {
+        document.getElementById('body-name').innerText = jstring.name;
+    }
+
+    // handle information in the General Info frame
+    let generalBox = document.querySelector('.general');
+
+    if(jstring.englishName == "Sun"){
+            let p = document.createElement('p');
+            generalBox.appendChild(p).innerHTML = "The Sun is the star at the center of the Solar System. It is by far the most important source of energy for life on Earth. ";
+        }
+
+    if(jstring.isPlanet == true){
+            let p = document.createElement('p');
+            generalBox.appendChild(p).innerHTML = jstring.englishName + " is a planet";
+    } else if (jstring.isPlanet == false && jstring.englishName !== "Sun") {
+            let p = document.createElement('p');
+            generalBox.appendChild(p).innerHTML = jstring.englishName + " is a moon of " + jstring.aroundPlanet.planet;
+    }
+
+    if(jstring.alternativeName !== ""){
+        let p = document.createElement('p');
+        generalBox.appendChild(p).innerHTML = "Alternative Name: " + jstring.alternativeName;
+    }
+
+    if(jstring.discoveredBy !== "") {
+        let p = document.createElement('p');
+        generalBox.appendChild(p).innerHTML = "Discovered By: " + jstring.discoveredBy;
+    }
+
+    if(jstring.discoveryDate !== "") {
+        let p = document.createElement('p');
+        generalBox.appendChild(p).innerHTML = "Discovery Date: " + jstring.discoveryDate;
+    }
+
+    // handle information in the Orbital Parameters frame
+    let orbital = document.querySelector('.orbital');
+
+    if(jstring.perihelion !== 0 && jstring.aphelion !== 0){
+        let p1 = document.createElement('p');
+        orbital.appendChild(p1).innerHTML = "Perihelion: " + jstring.perihelion + " km";
+        let p2 = document.createElement('p');
+        orbital.appendChild(p2).innerHTML = "Aphelion: " + jstring.aphelion + " km";
+    }
+
+    if(jstring.eccentricity !== ""){
+        let p = document.createElement('p');
+        orbital.appendChild(p).innerHTML = "Eccentricity: " + jstring.eccentricity;
+    }
+
+    if(jstring.sideralOrbit !== ""){
+        let p = document.createElement('p');
+        orbital.appendChild(p).innerHTML = "Orbital Period: " + jstring.sideralOrbit + " days";
+    }
+
+    if(jstring.inclination !== ""){
+        let p = document.createElement('p');
+        orbital.appendChild(p).innerHTML = "Inclination: " + jstring.inclination + " degrees";
+    }
+
+    // handle information in the Physical Properties frame
+    let physical = document.querySelector('.physical');
+
+    if(jstring.density !== ""){
+        let p = document.createElement('p');
+        physical.appendChild(p).innerHTML = "Mean Density: " + jstring.density + " g/cm"+ "<sup>3</sup>";
+    }
+
+    if(jstring.gravity !== ""){
+        let p = document.createElement('p');
+        physical.appendChild(p).innerHTML = "Gravity: " + jstring.gravity + " m/s"+ "<sup>2</sup>";
+    }
+
+    if(jstring.meanRadius !== ""){
+        let p = document.createElement('p');
+        physical.appendChild(p).innerHTML = "Mean Radius: " + jstring.meanRadius + " km";
+    }
+
+    if(jstring.vol.volValue !== ""){
+        let p = document.createElement('p');
+        physical.appendChild(p).innerHTML = "Volume: " + jstring.vol.volValue + " x 10"+"<sup>"+ jstring.vol.volExponent +"</sup>"+" km"+ "<sup>3</sup>";
+    }
+
+    if(jstring.mass.massValue !== ""){
+        let p = document.createElement('p');
+        physical.appendChild(p).innerHTML = "Mass: " + jstring.mass.massValue + " x 10"+"<sup>"+ jstring.mass.massExponent +"</sup>"+ " kg";
+    }
 
 }
 
 /* Display Response */
 
 function flip(el){
-    el.style.cssText = "transform: rotateY(180deg); transition: transform 1s linear";
+    el.style.cssText = "transform: rotateY(180deg); transition: transform .3s linear .2s";
 }
 
-function flipBack(el){
-    el.style.cssText = "transform: none; transition: transform 1s linear";
-}
+
